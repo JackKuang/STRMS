27ffc01e0e0fd9849636fc97269de7c0e1d1a606
Jack <1092465834@qq.com>
2016-12-03 23:40:14 +0800

    先还原简单的Dao层的实现。

--- src/main/java/com/hurenjieee/dao/LoginDao.java
+++ src/main/java/com/hurenjieee/dao/LoginDao.java
@@ -1,13 +1,23 @@
 package com.hurenjieee.dao;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.hurenjieee.entity.Student;
-import com.hurenjieee.util.BaseDao;
 
 @Repository
-public class LoginDao extends BaseDao<Student,Integer>{
+public class LoginDao {
+//public class LoginDao extends BaseDao<Student,Integer>{
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	public Session getSession() {
+		// 需要开启事物，才能得到CurrentSession
+		return sessionFactory.getCurrentSession();
+	}
 	
 	@Transactional
 	public void save(Student student)
