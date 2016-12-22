ad51389f4f4d2205b25521e47d8f635362069f05
JackKuang <1092465834@qq.com>
2016-12-22 17:32:03 +0800

    写了一点json，还有问题

--- src/main/java/com/hurenjieee/action/AdminAction.java
+++ src/main/java/com/hurenjieee/action/AdminAction.java
@@ -4,8 +4,12 @@ import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 
+import com.hurenjieee.entity.Branch;
+import com.hurenjieee.service.BranchService;
+import com.hurenjieee.service.MajorService;
 import com.hurenjieee.util.CRUDActionSupport;
 
 @ParentPackage(value = "all") // 应用全局包
