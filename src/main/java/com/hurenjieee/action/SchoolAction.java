3747408c56ff736277ff5ce04f65b147c423fb45
Jack <1092465834@qq.com>
2016-12-24 01:21:50 +0800

    更新代码，实现分院、专业的增加、修改

--- src/main/java/com/hurenjieee/action/SchoolAction.java
+++ src/main/java/com/hurenjieee/action/SchoolAction.java
@@ -13,24 +13,45 @@ import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
+import org.hibernate.type.StringRepresentableType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 
 import com.hurenjieee.entity.Branch;
 import com.hurenjieee.entity.Major;
+import com.hurenjieee.entity.Student;
 import com.hurenjieee.service.BranchService;
+import com.hurenjieee.service.CollectiveService;
+import com.hurenjieee.service.MajorService;
+import com.hurenjieee.service.StudentService;
+import com.hurenjieee.service.TeacherService;
 import com.hurenjieee.util.CRUDActionSupport;
+import com.sun.org.apache.xml.internal.resolver.helpers.PublicId;
 
 @ParentPackage(value = "json-default") // 应用全局包
 @Scope("prototype")
 @Namespace(value = "/admin")
 @Action(results = { @Result(name = "index", location = "/WEB-INF/jsp/admin/index.jsp"),
-		@Result(name = "branchList", type = "json", params = {"root", "resultMap","excludeProperties","^.*majors$"}) })
+		@Result(name = "json", type = "json", params = {"root", "resultMap"/*,"excludeProperties","^.*majors$"*/}) })
 public class SchoolAction extends CRUDActionSupport<Object> {
 
 	@Autowired
 	BranchService branchService;
 	
+	@Autowired
+	MajorService majorService;
+	
+	@Autowired
+	CollectiveService collectiveService;
+	
+	@Autowired
+	TeacherService teacherService;
+	
+	@Autowired
+	StudentService studentService;
+	
+	Branch branch;
+	Major major;
 	
 	Map<String, Object> resultMap;
 
@@ -41,15 +62,74 @@ public class SchoolAction extends CRUDActionSupport<Object> {
 	public void setResultMap(Map<String, Object> resultMap) {
 		this.resultMap = resultMap;
 	}
+	
+	public Branch getBranch() {
+		return branch;
+	}
+
+	public void setBranch(Branch branch) {
+		this.branch = branch;
+	}
+
+	public Major getMajor() {
+		return major;
+	}
+
+	public void setMajor(Major major) {
+		this.major = major;
+	}
 
 	public String branchList() throws IOException {
 		resultMap = new TreeMap<String, Object>();
 		List<Branch> list =  branchService.getList();
-		resultMap.put("key",list);
-		return "branchList";
+		resultMap.put("data",list);
+		return "json";
+	}
+	
+	public String saveBranch(){
+		resultMap = new TreeMap<String, Object>();
+		String result = (String)branchService.save(branch);
+		if ("".equals(result)) {
+			resultMap.put("result","defailed");
+		}else{
+			resultMap.put("result", "success");
+		}
+		return "json";
+	}
+	
+	public String updateBranch(){
+		resultMap = new TreeMap<String, Object>();
+		branchService.update(branch);
+		resultMap.put("result", "success");
+		return "json";
+	}
+	
+	public String MajorList(){
+		resultMap = new TreeMap<String, Object>();
+		List<Major> list =  majorService.getList();
+		resultMap.put("data",list);
+		return "json";		
 	}
 
 	public String index() {
 		return "index";
+	}
+
+	public String saveMajor(){
+		resultMap = new TreeMap<String, Object>();
+		String result = (String)majorService.save(major);
+		if ("".equals(result)) {
+			resultMap.put("result","defailed");
+		}else{
+			resultMap.put("result", "success");
+		}
+		return "json";
+	}
+	
+	public String updateMajor(){
+		resultMap = new TreeMap<String, Object>();
+		majorService.update(major);
+		resultMap.put("result", "success");
+		return "json";
 	}
 }
--- src/main/java/com/hurenjieee/service/MajorService.java
+++ src/main/java/com/hurenjieee/service/MajorService.java
@@ -7,15 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hurenjieee.entity.Log;
+import com.hurenjieee.entity.Major;
 import com.hurenjieee.util.BaseDao;
 
 @Service
 @Transactional
-public class MajorService extends BaseService<Log, String> {
+public class MajorService extends BaseService<Major, String> {
 	
 	@Autowired
 	public void setSessionFactory(SessionFactory sessionFactory) {
-		BaseDao<Log,String> dao = new BaseDao<Log,String>();
+		BaseDao<Major,String> dao = new BaseDao<Major,String>();
 		dao.setSessionFactory(sessionFactory);
 		setDao(dao);
 	}
