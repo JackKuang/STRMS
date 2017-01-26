cab7f962728f0b256883ab561e589817743bb7a1
JackKuang <1092465834@qq.com>
2017-01-26 16:22:31 +0800

    .

--- src/main/java/com/hurenjieee/action/LoginAction.java
+++ src/main/java/com/hurenjieee/action/LoginAction.java
@@ -15,95 +15,95 @@ import com.hurenjieee.util.CRUDActionSupport;
 
 @ParentPackage(value = "all") // 应用全局包
 @Scope("prototype")
-@Action(results = { @Result(name = "success-admin", type = "redirectAction", location = "admin/admin!index.action"),
-		@Result(name = "success-teacher", type = "redirectAction", location = "teacher/teacher!index.action"),
-		@Result(name = "success-student", type = "redirectAction", location = "student/student!index.action"),
-		@Result(name = "toLogin", location = "/WEB-INF/jsp/login.jsp") })
+@Action(results = { @Result(name = "success-admin",type = "redirectAction",location = "admin/admin!index.action"),
+        @Result(name = "success-teacher",type = "redirectAction",location = "teacher/teacher!index.action"),
+        @Result(name = "success-student",type = "redirectAction",location = "student/student!index.action"),
+        @Result(name = "toLogin",location = "/WEB-INF/jsp/login.jsp") })
 public class LoginAction extends CRUDActionSupport<Object> {
 
-	@Autowired
-	AdminService adminService;
+    @Autowired
+    AdminService   adminService;
 
-	@Autowired
-	TeacherService teacherService;
+    @Autowired
+    TeacherService teacherService;
 
-	@Autowired
-	StudentService studentService;
-	private String userName;
-	private String password;
-	private String userType;
+    @Autowired
+    StudentService studentService;
+    private String userName;
+    private String password;
+    private String userType;
 
-	public String getUserName() {
-		return userName;
-	}
+    public String getUserName(){
+        return userName;
+    }
 
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword(){
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password){
+        this.password = password;
+    }
 
-	public String getUserType() {
-		return userType;
-	}
+    public String getUserType(){
+        return userType;
+    }
 
-	public void setUserType(String userType) {
-		this.userType = userType;
-	}
+    public void setUserType(String userType){
+        this.userType = userType;
+    }
 
-	public String login() {
-		String result = "toLogin";
-		// 清理Session
-		getSessionMap().clear();
-		// 学生登录
-		if ("student".equals(userType)) {
-			Student student = studentService.getStudentByUsernameAndPassword(userName, password);
-			if (student != null) {
-				getSessionMap().put("userType", "student");
-				getSessionMap().put("student", student);
-				result = "success-admin";
-			} else {
-				getRequest().setAttribute("wrong", 1);
-				getRequest().setAttribute("userType", userType);
-				result = "toLogin";
-			}
-		}
-		// 教师登录
-		else if ("teacher".equals(userType)) {
-			Teacher teacher = teacherService.getTeacherByUsernameAndPassword(userName, password);
-			if (teacher != null) {
-				getSessionMap().put("userType", "teacher");
-				getSessionMap().put("student", teacher);
-				result = "success-admin";
-			} else {
-				getRequest().setAttribute("wrong", 1);
-				getRequest().setAttribute("type", userType);
-				result = "toLogin";
-			}
-		}
-		// 管理员登录
-		else if ("admin".equals(userType)) {
-			Admin admin = adminService.getAdminByUserNameAndPassword(userName, password);
-			if (admin != null) {
-				getSessionMap().put("userType", "admin");
-				getSessionMap().put("admin", admin);
-				result = "success-admin";
-			} else {
-				getRequest().setAttribute("wrong", 1);
-				getRequest().setAttribute("type", userType);
-				result = "toLogin";
-			}
-		}
-		return result;
-	}
+    public String login(){
+        String result = "toLogin";
+        // 清理Session
+        getSessionMap().clear();
+        // 学生登录
+        if ("student".equals(userType)) {
+            Student student = studentService.getStudentByUsernameAndPassword(userName,password);
+            if (student != null) {
+                getSessionMap().put("userType","student");
+                getSessionMap().put("student",student);
+                result = "success-admin";
+            } else {
+                getRequest().setAttribute("wrong",1);
+                getRequest().setAttribute("userType",userType);
+                result = "toLogin";
+            }
+        }
+        // 教师登录
+        else if ("teacher".equals(userType)) {
+            Teacher teacher = teacherService.getTeacherByUsernameAndPassword(userName,password);
+            if (teacher != null) {
+                getSessionMap().put("userType","teacher");
+                getSessionMap().put("student",teacher);
+                result = "success-admin";
+            } else {
+                getRequest().setAttribute("wrong",1);
+                getRequest().setAttribute("type",userType);
+                result = "toLogin";
+            }
+        }
+        // 管理员登录
+        else if ("admin".equals(userType)) {
+            Admin admin = adminService.getAdminByUserNameAndPassword(userName,password);
+            if (admin != null) {
+                getSessionMap().put("userType","admin");
+                getSessionMap().put("admin",admin);
+                result = "success-admin";
+            } else {
+                getRequest().setAttribute("wrong",1);
+                getRequest().setAttribute("type",userType);
+                result = "toLogin";
+            }
+        }
+        return result;
+    }
 
-	public String toLogin() {
-		return "toLogin";
-	}
+    public String toLogin(){
+        return "toLogin";
+    }
 }
--- src/main/java/com/hurenjieee/interceptor/RoleInterceptor.java
+++ src/main/java/com/hurenjieee/interceptor/RoleInterceptor.java
@@ -15,33 +15,33 @@ import com.opensymphony.xwork2.interceptor.Interceptor;
 @SuppressWarnings("serial")
 public class RoleInterceptor implements Interceptor {
 
-	@Override
-	public void destroy() {
+    @Override
+    public void destroy(){
 
-	}
+    }
 
-	@Override
-	public void init() {
+    @Override
+    public void init(){
 
-	}
+    }
 
-	@Override
-	public String intercept(ActionInvocation invocation) throws Exception {
-		// 增加返回结果前事件，实际开发过程中可以查看信息
-		// 取得请求相关的ActionContext实例  
-		ActionContext actionContext = invocation.getInvocationContext();  
-        Map<String,Object> session = actionContext.getSession();
+    @Override
+    public String intercept(ActionInvocation invocation) throws Exception{
+        // 增加返回结果前事件，实际开发过程中可以查看信息
+        // 取得请求相关的ActionContext实例
+        ActionContext actionContext = invocation.getInvocationContext();
+        Map<String, Object> session = actionContext.getSession();
         String userType = (String) session.get("userType");
-        HttpServletRequest httpServletRequest =  (HttpServletRequest)actionContext.get(ServletActionContext.HTTP_REQUEST);
+        HttpServletRequest httpServletRequest = (HttpServletRequest) actionContext.get(ServletActionContext.HTTP_REQUEST);
         String path = httpServletRequest.getContextPath();
-        System.out.println("11111111111111"+path);
+        System.out.println("11111111111111" + path);
         String servletPath = httpServletRequest.getServletPath();
-        //去除不需要过滤的网址
-        if("/login!login.action".equals(servletPath)
-        		|| "login!toLogin.action".equals(servletPath)){
-    		return invocation.invoke();        	
+        // 去除不需要过滤的网址
+        if ("/login!login.action".equals(servletPath) || "login!toLogin.action".equals(servletPath)) {
+            // 经过Servlet方法
+            return invocation.invoke();
         }
         return Action.LOGIN;
-	}
+    }
 
 }
